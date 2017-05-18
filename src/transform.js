ea89418925898f51bde08bfab2120b43227ab4b7
differui <differui@gmail.com>
2017-05-18 14:08:16 +0800

    Add dot member expression transformer

--- src/transform.js
+++ src/transform.js
@@ -1,16 +1,57 @@
 import { parse } from 'esprima'
 
-// function replace
+let source
+const records = []
+
+function pluck() {
+  parse(source, {
+    range: true,
+    module: true,
+  }, (n) => {
+    if (n.type !== 'MemberExpression') {
+      return
+    }
+
+    if (n.property.type !== 'Literal') {
+      records.push({
+        start: n.object.range[0],
+        end: n.object.range[1],
+      })
+      records.push({
+        start: n.property.range[0],
+        end: n.property.range[1],
+      })
+    }
+  })
+}
+
+function tailor() {
+  let propertyRecord = records.pop()
+  let objectRecord = records.pop()
+
+  while (propertyRecord && objectRecord) {
+    const object = source.substring(objectRecord.start, objectRecord.end)
+    const property = source.substring(propertyRecord.start, propertyRecord.end)
+
+    source = [
+      source.substring(0, objectRecord.start),
+      `${object}['${property}']`,
+      source.substring(propertyRecord.end),
+    ].join('')
+
+    propertyRecord = records.pop()
+    objectRecord = records.pop()
+  }
+}
 
 function main(code) {
-  const ast = parse(code, {
-    loc: true,
-    range: true,
-    comment: true,
-    module: true,
-  })
+  source = code
+  records.length = 0
 
-  console.log(ast.body[3])
+  pluck()
+  tailor()
+
+  return source
 }
 
 export const transform = main
--- test/index.js
+++ test/index.js
@@ -2,10 +2,16 @@ import { readFileSync } from 'fs'
 import test from 'ava'
 import { transform } from '../'
 
-test('should transform basic code', t => {
-  const actual = readFileSync('test/fixtures/basic/actual.js').toString()
-  const expect = readFileSync('test/fixtures/basic/expect.js').toString()
+test('should transform dot member expression', t => {
+  const actual = readFileSync('test/fixtures/dot-member-expression/actual.js').toString()
+  const expect = readFileSync('test/fixtures/dot-member-expression/expect.js').toString()
 
   t.is(expect, transform(actual))
 })
 
+test('should not transform bracket member expression', t => {
+  const actual = readFileSync('test/fixtures/bracket-member-expression/actual.js').toString()
+  const expect = readFileSync('test/fixtures/bracket-member-expression/expect.js').toString()
+
+  t.is(expect, transform(actual))
+})
